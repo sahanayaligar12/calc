715c77ce7c512596399875c4ad2b20a2c4731d1e
sahanayaligar12 <yaligarsahana5@gmail.com>
2025-05-06 10:09:15 +0530

    save

---
+++ DivOpn.java
@@ -0,0 +1,17 @@
+package Calc;
+
+public class DivOpn {
+    public static void main(String[] args) {
+        int a=15;
+        int b=0;
+        try{
+            int div=a/b;
+            System.out.println("Division is: "+div);
+        } catch(ArithmeticException e){
+            System.out.println("Error:Cannot divide by zero");
+    }
+}
+}
+
+
+
